058e2245f47f6a61cb0899d2019fbe25dbad8344
DevAnsar <ansarmirzayi@gmail.com>
2024-07-18 11:25:24 +0330

    Workflow

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://devansar.github.io/address-getter/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>
