47613e037cbe40f613a259a0faace3f8577f9c78
DevAnsar <ansarmirzayi@gmail.com>
2024-07-20 01:02:26 +0330

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(), basicSsl()],
+  plugins: [react(), nodePolyfills()],
   base: "/address-getter",
 });
